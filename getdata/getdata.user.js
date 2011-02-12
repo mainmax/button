6244faecf9f4a3f7e6abea3426a94d3392281663
Max <max@max-eee.(none)>
2011-02-11 23:08:33 -0500

    Tested on AES, Creates <input> if missing

--- getdata/getdata.user.js
+++ getdata/getdata.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name           getdata
-// @namespace      getdata
-// @description    Retrive Data through Iframe
+// @name           aes
+// @namespace      aes
+// @include        *
 // ==/UserScript==
 
 var d= unsafeWindow.document;
@@ -9,40 +9,44 @@ window.d = d;
 
 if (String(d.location).indexOf('127.0.0.1')>0 || String(d.location).indexOf('aesdirect.gov')>0)  {
 
-var info = d.createElement("p");
-info.innerHTML = "this is " + d.location;
-d.body.appendChild(info);
-
 var button = d.createElement("a");
-button.innerHTML = "BUTTON";
+button.innerHTML = "LOAD";
 button.href = "javascript: DoTheMagic();";
-d.body.appendChild(button);
+d.getElementById("shipmentreferencenumber").parentNode.appendChild(button);
 
 unsafeWindow.Propagate = function () {
-	var varlist = String(GM_getValue("varlist")).split('|');
-//	var sss = "DATA<br>";	
-//	sss += GM_getValue("varlist")+"<br>";
-	for (key in varlist) {
-	//	alert(varlist[key]);
-		if (document.getElementsByName(varlist[key])) {
-			document.getElementsByName(varlist[key])[0].value = GM_getValue(varlist[key],"default");
-		}
-//		sss += varlist[key]+" = "+GM_getValue(varlist[key],"***")+"<br>";
-	}
-//	var ppp = d.createElement("p");
-//	ppp.innerHTML = sss;
-//	d.body.appendChild(ppp);
+    var varlist = String(GM_getValue("varlist")).split('|');
+//    var sss = "DATA<br>";   
+//    sss += GM_getValue("varlist")+"<br>";
+    for (key in varlist) {
+//        alert(varlist[key]);
+        if (document.getElementsByName(varlist[key]).length > 0) {
+            document.getElementsByName(varlist[key])[0].value = GM_getValue(varlist[key],"default");
+        } else {
+            var input = document.createElement("input");
+            input.name = varlist[key];
+            input.type = "HIDDEN";
+            input.value = GM_getValue(varlist[key],"default");
+            document.getElementsByName("edit")[0].appendChild(input);
+        }
+//        sss += varlist[key]+" = "+GM_getValue(varlist[key],"***")+"<br>";
+    }
+//    var ppp = d.createElement("p");
+//    ppp.innerHTML = sss;
+//    d.body.appendChild(ppp);
 }
 
 unsafeWindow.DoTheMagic = function (){
-	var iframe = d.createElement("iframe");
-	iframe.onload = unsafeWindow.Propagate;
-	iframe.src = "http://unitedmotorsjax.com/data/amr062";
-	unsafeWindow.iframe = iframe;
-	d.body.appendChild(iframe);
+    var ref = d.getElementById("shipmentreferencenumber").value;
+    var iframe = d.createElement("iframe");
+    iframe.onload = unsafeWindow.Propagate;
+    iframe.src = "http://unitedmotorsjax.com/data/"+ref;
+    unsafeWindow.iframe = iframe;
+    d.body.appendChild(iframe);
 }
 
 }
+
 
 if (String(d.location).indexOf('unitedmotorsjax.com')>0)  {
 
@@ -50,19 +54,12 @@ var dataform = d.getElementsByName("data")[0].elements;
 var varlist = "";
 
 for (var item=0; item < dataform.length; item++) {
-	varlist += dataform[item].name+"|";
-	GM_setValue(dataform[item].name,dataform[item].value);	
+    varlist += dataform[item].name+"|";
+    GM_setValue(dataform[item].name,dataform[item].value);   
 }
 varlist = varlist.substr(0, varlist.length-1); // Delete trailing '|'
 
-GM_setValue("varlist",varlist);	// varlist contains key names of parameters to pass.
-
-var info = d.createElement("p");
-info.innerHTML = "this is " + d.location;
-d.body.appendChild(info);
-
+GM_setValue("varlist",varlist);    // varlist contains key names of parameters to pass.
 }
-
-
 
 
